239f251508d9e2e9e0f78dc1a3e1e9d6b5731b96
Jonathan Ekberg <jodrim@gmail.com>
2024-01-17 13:58:01 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    cpus: 8,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,91 @@
-import Image from 'next/image'
+import Image from "next/image";
+import { createConnection } from "mysql2/promise";
 
-export default function Home() {
+export interface RFAM {
+  rfam_acc: string;
+  rfam_id: string;
+  auto_wiki: number;
+  description: string;
+  author: string;
+  seed_source: string;
+  gathering_cutoff: number;
+  trusted_cutoff: number;
+  noise_cutoff: number;
+  comment: string;
+  previous_id: any;
+  cmbuild: string;
+  cmcalibrate: string;
+  cmsearch: string;
+  num_seed: number;
+  num_full: number;
+  num_genome_seq: number;
+  num_refseq: number;
+  type: string;
+  structure_source: string;
+  number_of_species: number;
+  number_3d_structures: number;
+  num_pseudonokts: any;
+  tax_seed: string;
+  ecmli_lambda: number;
+  ecmli_mu: number;
+  ecmli_cal_db: number;
+  ecmli_cal_hits: number;
+  maxl: number;
+  clen: number;
+  match_pair_node: number;
+  hmm_tau: number;
+  hmm_lambda: number;
+  created: string;
+  updated: string;
+}
+
+export default async function Home() {
+  /**
+   * wtf
+   * "Rfam is a database containing information about non-coding RNA families and other structured RNA elements."
+   * https://docs.rfam.org/en/latest/database.html
+   */
+  const connection = await createConnection({
+    host: "mysql-rfam-public.ebi.ac.uk",
+    user: "rfamro",
+    port: 4497,
+    database: "Rfam",
+    password: undefined,
+  });
+  const [countQuery, result] = await Promise.all([
+    connection.query(`SELECT COUNT(rfam_id) as row_count FROM family;`),
+    connection.query(`SELECT * FROM family LIMIT 1`),
+  ]);
+  const val = result[0] as RFAM[];
+  const count = (countQuery[0] as { row_count: number }[])[0]["row_count"];
+  console.log(count);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+        <div>
+          Rows: {count}
+          <div>{val.at(0)!.rfam_acc}</div>
+          <div>{val.at(0)!.comment}</div>
+        </div>
+        <div>
+          {Array(0)
+            .fill(null)
+            .map((_, idx) => (
+              <div
+                key={idx}
+                className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+              >
+                Get started by eating&nbsp; Get started by editing&nbsp; Get
+                started Get started by eating&nbsp; Get started by editing&nbsp;
+                Get started by editing&nbsp; lol by editing&nbsp; lol
+                <code className="font-mono font-bold">src/app/page.tsx</code>
+                <code className="font-mono font-bold">src/app/page.tsx</code>
+                <code className="font-mono font-bold">src/app/page.tsx</code>
+                <code className="font-mono font-bold">src/app/page.tsx</code>
+              </div>
+            ))}
+        </div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -15,7 +93,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            {/* By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -23,20 +101,20 @@ export default function Home() {
               width={100}
               height={24}
               priority
-            />
+            /> */}
           </a>
         </div>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
+        {/* <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
           alt="Next.js Logo"
           width={180}
           height={37}
           priority
-        />
+        /> */}
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -47,7 +125,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
+            Docs{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -64,7 +142,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
+            Learn{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -81,7 +159,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
+            Templates{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -98,7 +176,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
+            Deploy{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
@@ -109,5 +187,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
