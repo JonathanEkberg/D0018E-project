3b1fa03cb94c93d3765b2cae0f9a65554901d264
Jonathan Ekberg <jodrim@gmail.com>
2024-01-17 15:47:30 +0100

    Add initial database schema SQL script

---
+++ database.sql
@@ -0,0 +1,83 @@
+CREATE TABLE `user` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `role` ENUM ('USER', 'ADMIN') NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) UNIQUE NOT NULL,
+  `password` varchar(255) NOT NULL
+);
+
+CREATE TABLE `product` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255),
+  `image` varchar(255),
+  `price_usd` DECIMAL UNSIGNED,
+  `stock` int UNSIGNED
+);
+
+CREATE TABLE `order` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` integer
+);
+
+CREATE TABLE `product_order` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `amount` integer,
+  `order_id` integer,
+  `product_id` integer
+);
+
+CREATE TABLE `review` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `stars` integer,
+  `text` varchar(255),
+  `user_id` integer,
+  `product_id` integer
+);
+
+CREATE TABLE `shopping_cart` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `user_id` integer UNIQUE
+);
+
+CREATE TABLE `shopping_cart_item` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `shopping_cart_id` integer,
+  `product_id` integer
+);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`id`) REFERENCES `shopping_cart` (`user_id`);
+
+ALTER TABLE `shopping_cart_item` ADD FOREIGN KEY (`shopping_cart_id`) REFERENCES `shopping_cart` (`id`);
+
+ALTER TABLE `shopping_cart_item` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `product_order` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `product_order` ADD FOREIGN KEY (`order_id`) REFERENCES `order` (`id`);
+
+ALTER TABLE `order` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `review` ADD FOREIGN KEY (`product_id`) REFERENCES `product` (`id`);
+
+ALTER TABLE `review` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+
+ALTER TABLE `product_order` ADD FOREIGN KEY (`amount`) REFERENCES `product_order` (`order_id`);
+
+ALTER TABLE `product` ADD FOREIGN KEY (`id`) REFERENCES `product` (`created_at`);
+
+ALTER TABLE `product_order` ADD FOREIGN KEY (`created_at`) REFERENCES `product_order` (`amount`);
