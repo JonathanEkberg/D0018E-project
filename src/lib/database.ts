b12eea99a0dd874d3835fb7f5b0f711b95d6cd6a
Jonathan Ekberg <jodrim@gmail.com>
2024-01-18 13:39:41 +0100

    Log opts

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,11 +1,13 @@
-import { createConnection } from "mysql2/promise";
+import { ConnectionOptions, createConnection } from "mysql2/promise";
 
 export function createDb() {
-  return createConnection({
+  const opts: ConnectionOptions = {
     connectionLimit: 20,
     host: process.env.HOST as string,
     user: process.env.USER as string,
     database: process.env.DATABASE as string,
     password: process.env.PASSWORD as string,
-  });
+  };
+  console.log(opts);
+  return createConnection(opts);
 }
