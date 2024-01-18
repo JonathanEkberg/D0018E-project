ce44e1203cb11a4434dade9902350695f0d44722
Jonathan Ekberg <jodrim@gmail.com>
2024-01-18 13:41:47 +0100

    Fix env

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,10 +3,10 @@ import { ConnectionOptions, createConnection } from "mysql2/promise";
 export function createDb() {
   const opts: ConnectionOptions = {
     connectionLimit: 20,
-    host: process.env.HOST as string,
-    user: process.env.USER as string,
-    database: process.env.DATABASE as string,
-    password: process.env.PASSWORD as string,
+    host: process.env.DB_HOST as string,
+    user: process.env.DB_USER as string,
+    database: process.env.DB_DATABASE as string,
+    password: process.env.DB_PASSWORD as string,
   };
   console.log(opts);
   return createConnection(opts);
