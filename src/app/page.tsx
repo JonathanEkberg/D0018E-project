f0760f4072879218b57df1ddcb4c42e94fd8a61a
Jonathan Ekberg <jodrim@gmail.com>
2024-01-18 13:34:28 +0100

    Force SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { CreateProduct } from "@/components/CreateProduct";
 import { ProductList } from "@/components/ProductList";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   return (
     <main className="flex min-h-screen max-w-xl flex-col items-center justify-between p-24 mx-auto">
